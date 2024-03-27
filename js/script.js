073261a11f9acafd314fc2ceecac129fa42d3877
mendesmariac <marrferra01@gmail.com>
2024-03-27 18:56:46 -0300

    código finalizado uhuu

--- js/script.js
+++ js/script.js
@@ -1,38 +1,47 @@
 const form = document.getElementById ('form');
 
-form.addEventListener('submit', function(event){
+form.addEventListener ('submit', (event) => {
     event.preventDefault();
-    
-    const weight = document.getElementById ('weight').value;
-    const height = document.getElementById ('height').value;
 
-    const bmi = (weight / (height*height)).toFixed(2);
+    const weigth = Number(document.getElementById ('weight').value);
 
-    const value = document.getElementById('value');
-    let description = '';
+    const height = Number(document.getElementById ('height').value);
 
-    value.classList.add ('attention');
 
-    document.getElementById('infos').classList.remove('hidden');
+    if(isNaN(weigth) || isNaN(height)) {
 
-    if (bmi< 18.5) {
-        description = 'Cuidado! você está abaixo do peso!';
-    } else if (bmi >= 18.5 && bmi <= 25) {
-        description = 'Você está no peso ideal!';
-        value.classList.remove ('attention');
-        value.classList.add ('normal');
+        alert("Digite novamente")
 
-    } else if (bmi>25 && bmi <=30){
-        description = 'Cuidado! você está com sobrepeso!';
-    } else if (bmi > 30 && bmi<=35) {
-        description = 'Cuidado! você está com obesidade moderada!';
-    } else if (bmi>35 && bmi <=40) {
-        description = 'Cuidado! você está com opesidade severa!';
-    } else {
-        description = 'Cuidado! você está com obesidade morbida!';
+        return
     }
 
-    value.textContent = bmi.replace('.',',');
+    const bmi = (weigth / (height*height)).toFixed (2);
+
+    const value = document.getElementById ('value');
+    let description = '';
+
+    value.classList.add('normal');
+
+
+    document.getElementById ('infos'). classList.remove ('hidden');
+
+    if (bmi < 18.5) {
+        description = 'Cuidado! você está abaixo do peso!';
+        value.classList.add('attention');
+    } else if (bmi >= 18.5 && bmi <= 25) {
+        description = 'Parabéns, você está com o peso ideal!';
+    } else if (bmi > 25 && bmi <= 30 ) {
+        description = 'Cuidado! você está com sobrepeso!';
+        value.classList.add('attention');
+    } else if (bmi > 30 && bmi <= 40) {
+        description = 'Cuidado! você está com obesidade severa!';
+        value.classList.add('attention');
+    } else {
+        description = 'Cuidado! você está com obesidade mórbida!'
+        value.classList.add('attention');
+    }
+    value.textContent = bmi.replace ('.', ',');
     document.getElementById ('description').textContent = description;
+    
 
 });
